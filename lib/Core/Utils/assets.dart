9d283d835ac03004f9f9e6d5f7bdc1a56f38791b
hotheefa <77983378+hotheefa@users.noreply.github.com>
2025-01-05 19:25:34 +0300

    .

---
+++ lib/Core/Utils/assets.dart
@@ -0,0 +1,29 @@
+
+class AssestData{
+static const String Logo = 'assets/logooPng.png';
+static const String arraw_rajhit = 'assets/Icon/arraw_rajhit.svg';
+
+static const String wallet = "assets/Icon/wallet (1).svg";
+static const String folder_open = "assets/Icon/folder-open.svg";
+static const String document = "assets/Icon/document.svg";
+static const String task_square = "assets/Icon/task-square.svg";
+static const String arrow_left = "assets/Icon/arrow-left.svg";
+static const String clear = "assets/Icon/clear.png";
+static const String clear2 = "assets/Icons/clear.png";
+static const String left = "assets/Icons/left.svg";
+static const String my_project = "assets/Icon/home-hashtag.svg";
+static const String my_service = "assets/Icon/hashtag.svg";
+static const String my_bookmark = "assets/Icon/bookmark.svg";
+static const String my_chart = "assets/Icon/chart-2.svg";
+static const String arrow_left2 = "assets/Icon/arrow-left2.svg";
+static const String time =  "assets/Icons/Exclude.svg";
+static const String uplode =  "assets/Icons/uplode.svg";
+static const String back =  "assets/Icon/Line arrow-rightttt.svg";
+static const String languge =   "assets/Icon/GlobeSimple.svg";
+static const String wizard1 =   "assets/wizard1.svg";
+static const String wizard2 =   "assets/wizard2.svg";
+static const String wizard3 =   "assets/wizard3.svg";
+static const String wizard4 =   "assets/wizard4.svg";
+static const String arrow_square_left =   "assets/Icon/arrow-square-left.svg";
+
+}
